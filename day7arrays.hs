f7312054f41f41cfb7f3b50b3459c6cf14042e77
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2018-02-11 03:07:27 -0500

    Update day7arrays.hs

--- day7arrays.hs
+++ day7arrays.hs
@@ -2,11 +2,9 @@ import System.IO
 import Data.List
 main :: IO ()
 main = do
-    -- n_temp <- getLine
-    let n_temp="2"
+    n_temp <- getLine
     let n = read n_temp :: Int
-    -- arr_temp <- getLine
-    let arr_temp = "1 4 3 2"
+    arr_temp <- getLine
     let arr = words arr_temp
     let rarr = intercalate " " $reverse arr
     putStrLn rarr
